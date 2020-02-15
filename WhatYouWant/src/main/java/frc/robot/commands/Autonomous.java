17d7d6f9854b24aef2f58582f3a24f00d2416281
rbibby53 <rbibby53@gmail.com>
2020-02-15 17:09:39 -0500

    autonomous?
    added timer stuff

--- WhatYouWant/src/main/java/frc/robot/commands/Autonomous.java
+++ WhatYouWant/src/main/java/frc/robot/commands/Autonomous.java
@@ -1,5 +1,6 @@
 package frc.robot.commands; 
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.RobotContainer;
 import frc.robot.subsystems.DriveTrain; 
@@ -7,6 +8,7 @@ import frc.robot.subsystems.DriveTrain;
 public class Autonomous extends SequentialCommandGroup{
     public Autonomous(DriveTrain drive){
         RobotContainer.differential.differentialDrive(10, 10);
+        Timer.delay(3);
         //addCommands();
     }
 

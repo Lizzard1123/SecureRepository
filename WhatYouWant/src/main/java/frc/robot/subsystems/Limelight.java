ae3be9dc05b098e87cb095f43c98afa68385de09
rbibby53 <rbibby53@gmail.com>
2020-02-29 10:05:16 -0500

    added limelight class

---
+++ WhatYouWant/src/main/java/frc/robot/subsystems/Limelight.java
@@ -0,0 +1,16 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.networktables.EntryListenerFlags;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+//import frc.robot.lib.motion.Util;
+//import frc.robot.lib.obj.VisionTarget;
+//import frc.robot.lib.obj.factories.VisionTargetFactory;
+
+public class Limelight extends SubsystemBase{
+
+    
+}
